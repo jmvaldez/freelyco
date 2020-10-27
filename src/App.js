d114733f1ae1f03e2fbd5e8afa8df3ca89eb013d
joseph <joseph.m.valdez1@gmail.com>
2020-10-26 19:15:04 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckOutForm";
 
-function App() {
+const stripePromise = loadStripe("pk_test_51HMQW2CeaSGVLhh56cKOf66VR380wDCHM1yPj7K3TGRpnySCFEuWirSjJd3uWfBVj2A66XMhy1QGTNTLafVRjNX500XtqeTBY7");
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="product">
+        <img
+          src="https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress"
+          alt="laptop"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div>
+          <Elements stripe={stripePromise}>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
