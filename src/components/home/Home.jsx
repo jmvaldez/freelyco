e14bbe5a1dcb55c0d22edd76b05b80192dc51cce
joseph <joseph.m.valdez1@gmail.com>
2020-11-08 21:30:10 -0800

    Homepage cards WIP

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,8 +3,26 @@ import "./HomeStyles.css";
 import CampingImage from "../../layout/layoutImages/campingPlaceHolder.jpg";
 
 export const Home = (props) => (
-  <div className="shopCategoryContainer">
-    <input type="image" src={CampingImage} alt="" />
-    <h1 className="imageLabel">Camping</h1>
+  <div className="row">
+    <div className="column">
+      <div className="card">
+        <h1 className="imageLabel">Camping</h1>
+        <input type="image" src={CampingImage} alt="" />
+      </div>
+    </div>
+
+    <div className="column">
+      <div className="card">
+        <h1 className="imageLabel">Clothing</h1>
+        <input type="image" src={CampingImage} alt="" />
+      </div>
+    </div>
+
+    <div className="column">
+      <div className="card">
+        <h1 className="imageLabel">Footwear</h1>
+        <input type="image" src={CampingImage} alt="" />
+      </div>
+    </div>
   </div>
 );
